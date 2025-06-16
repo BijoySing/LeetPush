4b4a54d8fd23b150195a06dc28fc8f65a576007f
Bijoy Sing <70032841+BijoySing@users.noreply.github.com>
2025-06-16 10:55:19 +0600

    Time: 0 ms (100.00%) | Memory: 11.8 MB (71.76%) - LeetSync

--- 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.cpp
+++ 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.cpp
@@ -3,12 +3,8 @@ public:
     int maximumDifference(vector<int>& nums) {
         int mx = -1;
         int b = nums[0];
-        vector<int> v = nums;
-        sort(v.rbegin(), v.rend());
-        if (v == nums)
-            return -1;
-        for (int i = 0; i < nums.size(); i++) {
-            mx = max(mx, nums[i] - b);
+            for (int i = 1; i < nums.size(); i++) {
+           if(nums[i]>b) mx = max(mx, nums[i] - b);
             b = min(b, nums[i]);
         }
         return mx;
