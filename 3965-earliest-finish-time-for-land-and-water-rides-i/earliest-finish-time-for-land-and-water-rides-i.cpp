c69f59ca70dbce370a64e8b9821cc675f907bc06
Bijoy Sing <70032841+BijoySing@users.noreply.github.com>
2026-06-02 09:27:26 +0600

    Time: 13 ms (27.85%) | Memory: 91.8 MB (59.92%) - LeetSync

--- 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.cpp
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.cpp
@@ -7,14 +7,18 @@ public:
 
         for (int i = 0; i < lst.size(); i++) {
             int t = lst[i] + ldu[i];
-            for (int j = 0; j < wst.size(); j++)
+
+            for (int j = 0; j < wst.size(); j++) {
                 ans = min(ans, max(t, wst[j]) + wdu[j]);
+            }
         }
 
         for (int i = 0; i < wst.size(); i++) {
             int t = wst[i] + wdu[i];
-            for (int j = 0; j < lst.size(); j++)
+
+            for (int j = 0; j < lst.size(); j++) {
                 ans = min(ans, max(t, lst[j]) + ldu[j]);
+            }
         }
 
         return ans;
