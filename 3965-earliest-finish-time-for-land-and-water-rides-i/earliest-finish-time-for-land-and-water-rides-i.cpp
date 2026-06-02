52b7e5a37aff353732c9b870b65b2bb0d935a845
Bijoy Sing <70032841+BijoySing@users.noreply.github.com>
2026-06-02 09:17:58 +0600

    Time: 11 ms (38.40%) | Memory: 91.9 MB (59.92%) - LeetSync

--- 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.cpp
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
-    int earliestFinishTime(vector<int>& ls, vector<int>& ld, vector<int>& ws,
-                           vector<int>& wd) {
-        vector<int> ans;
-        for (int i = 0; i < ls.size(); i++) {
-            int t = ls[i] + ld[i];
-            for (int j = 0; j < ws.size(); j++) {
-                int tt = t;
-                if (tt >= ws[j])
-                    tt += wd[j];
-                else
-                    tt = ws[j] + wd[j];
-                ans.push_back(tt);
-            }
+    int earliestFinishTime(vector<int>& lst, vector<int>& ldu,
+                           vector<int>& wst, vector<int>& wdu) {
+        
+        int ans = INT_MAX;
+
+        for (int i = 0; i < lst.size(); i++) {
+            int t = lst[i] + ldu[i];
+            for (int j = 0; j < wst.size(); j++)
+                ans = min(ans, max(t, wst[j]) + wdu[j]);
         }
-         for (int i = 0; i < ws.size(); i++) {
-            int t = ws[i] + wd[i];
-            for (int j = 0; j <ls.size(); j++) {
-                int tt = t;
-                if (tt >= ls[j])
-                    tt += ld[j];
-                else
-                    tt = ls[j] + ld[j];
-                ans.push_back(tt);
-            }
+
+        for (int i = 0; i < wst.size(); i++) {
+            int t = wst[i] + wdu[i];
+            for (int j = 0; j < lst.size(); j++)
+                ans = min(ans, max(t, lst[j]) + ldu[j]);
         }
-        sort(ans.begin(),ans.end());
-        return ans[0];
+
+        return ans;
     }
 };
