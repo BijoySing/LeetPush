ff713fd064637ed391d6e540cb3e907bd0ae8726
Bijoy Sing <70032841+BijoySing@users.noreply.github.com>
2026-06-06 09:42:06 +0600

    Time: 0 ms (100.00%) | Memory: 15 MB (63.06%) - LeetSync

--- 2714-left-and-right-sum-differences/left-and-right-sum-differences.cpp
+++ 2714-left-and-right-sum-differences/left-and-right-sum-differences.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
     vector<int> leftRightDifference(vector<int>& nums) {
-        vector<int> pre, suf, ans;
-        int s = 0;
-        int sum = accumulate(nums.begin(), nums.end(), 0);
-        for (int i = 0; i < nums.size() - 1; i++) {
-            pre.push_back(s);
-            s += nums[i];
-        }
-        pre.push_back(s);
+        int n = nums.size();
+        vector<int> ans(n);
 
-        s = 0;
-        for (int i = 1; i < nums.size(); i++) {
-            s = sum - pre[i];
-            suf.push_back(s);
-        }
-        suf.push_back(0);
+        int rsum = accumulate(nums.begin(), nums.end(), 0);
+        int lsum = 0;
 
-        for (int i = 0; i < nums.size(); i++) {
-            ans.push_back(abs(pre[i] - suf[i]));
+        for (int i = 0; i < n; i++) {
+            rsum -= nums[i];
+            ans[i] = abs(lsum - rsum);
+            lsum += nums[i];
         }
+
         return ans;
     }
 };
