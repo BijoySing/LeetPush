2c7a2d1c69245ca392a8499e0a22e7de60768b12
Bijoy Sing <70032841+BijoySing@users.noreply.github.com>
2026-06-20 07:22:50 +0600

    Time: 49 ms (86.11%) | Memory: 106.7 MB (38.89%) - LeetSync

--- 1968-maximum-building-height/maximum-building-height.cpp
+++ 1968-maximum-building-height/maximum-building-height.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
     int maxBuilding(int n, vector<vector<int>>& r) {
-        if(r.empty()) return n - 1;
-
+        r.push_back({1,0});
+        r.push_back({n,n-1});
         sort(r.begin(), r.end());
 
-        int id = 1, h = 0;
+        for(int i = 1; i < r.size(); i++)
+            r[i][1] = min(r[i][1], r[i-1][1] + r[i][0] - r[i-1][0]);
 
-        for(auto &x : r) {
-            x[1] = min(x[1], x[0] - id + h);
-            id = x[0];
-            h = x[1];
-        }
-
-        for(int i = r.size() - 2; i >= 0; i--) {
+        for(int i = r.size()-2; i >= 0; i--)
             r[i][1] = min(r[i][1], r[i+1][1] + r[i+1][0] - r[i][0]);
-        }
 
-        int ans = n - r.back()[0] + r.back()[1];
-        id = 1; h = 0;
-
-        for(auto &x : r) {
-            int steps = x[0] - id - abs(x[1] - h);
-            int higher = max(x[1], h);
-            ans = max(ans, higher + steps / 2);
-            id = x[0];
-            h = x[1];
+        int ans = 0;
+        for(int i = 0; i + 1 < r.size(); i++) {
+            int d = r[i+1][0] - r[i][0];
+            ans = max(ans, (r[i][1] + r[i+1][1] + d) / 2);
         }
 
         return ans;
