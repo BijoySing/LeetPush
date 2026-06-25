69cdb935b8594d8595f56846d9cca523b48fe365
Bijoy Sing <70032841+BijoySing@users.noreply.github.com>
2026-06-25 07:08:30 +0600

    Time: 0 ms (100.00%) | Memory: 95.6 MB (88.16%) - LeetSync

--- 4075-count-subarrays-with-majority-element-ii/count-subarrays-with-majority-element-ii.cpp
+++ 4075-count-subarrays-with-majority-element-ii/count-subarrays-with-majority-element-ii.cpp
@@ -1,29 +1,23 @@
-#include <ext/pb_ds/assoc_container.hpp>
-using namespace __gnu_pbds;
-
-template<typename T>
-using ordered_set =
-tree<T, null_type, less<T>, rb_tree_tag,
-tree_order_statistics_node_update>;
-
 class Solution {
 public:
     long long countMajoritySubarrays(vector<int>& nums, int target) {
-        long long ans = 0;
-        int pref = 0;
-        int id = 0;
-
-        ordered_set<pair<int,int>> st;
-        st.insert({0, id++});
-
-        for (int x : nums) {
-            pref += (x == target ? 1 : -1);
-
-            ans += st.order_of_key({pref, -1});
-
-            st.insert({pref, id++});
+        int n = nums.size();
+         vector<int> pre(n * 2 + 1, 0);
+        pre[n] = 1;
+        int cnt = n;
+        long long ans = 0, presum = 0;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] == target) {
+                presum += pre[cnt];
+                ++cnt;
+                ++pre[cnt];
+            } else {
+                --cnt;
+                presum -= pre[cnt];
+                ++pre[cnt];
+            }
+            ans += presum;
         }
-
         return ans;
     }
 };
