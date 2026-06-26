6949312f520e7052922992aa858f2ca8d3acd7d0
Bijoy Sing <70032841+BijoySing@users.noreply.github.com>
2026-06-26 07:48:01 +0600

    Time: 4 ms (95.02%) | Memory: 41.6 MB (26.84%) - LeetSync

--- 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.cpp
+++ 4074-count-subarrays-with-majority-element-i/count-subarrays-with-majority-element-i.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     long long countMajoritySubarrays(vector<int>& nums, int target) {
-        int n = nums.size();
-         vector<int> pre(n * 2 + 1, 0);
-        pre[n] = 1;
-        int cnt = n;
-        long long ans = 0, presum = 0;
-        for (int i = 0; i < n; ++i) {
-            if (nums[i] == target) {
-                presum += pre[cnt];
-                ++cnt;
-                ++pre[cnt];
-            } else {
-                --cnt;
-                presum -= pre[cnt];
-                ++pre[cnt];
-            }
-            ans += presum;
+        int size = nums.size(), pref = size;
+
+        vector<int> freq(2 * size + 1);
+        freq[size] = 1;
+
+        long long less = 0, ans = 0;
+
+        for (int num : nums) {
+            if (num == target)
+                less += freq[pref++];
+            else
+                less -= freq[--pref];
+
+            ++freq[pref];
+            ans += less;
         }
+
         return ans;
     }
 };
