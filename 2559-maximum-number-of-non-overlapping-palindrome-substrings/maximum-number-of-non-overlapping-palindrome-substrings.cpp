88986b94d35f361eec8e2d3eda1b711d8452e79a
Bijoy Sing <70032841+BijoySing@users.noreply.github.com>
2026-09-15 06:31:31 +0600

    Time: 0 ms (100.00%) | Memory: 8.7 MB (100.00%) - LeetSync

--- 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
     int maxPalindromes(string s, int k) {
-    int n = s.size(), last = INT_MIN, ans = 0;
-    vector<vector<int>> intervals;
-    for (int center = 0; center < 2 * n; center++) {
-        int left = center / 2;
-        int right = left + center % 2;
-        while (left >= 0 && right < n && s[left] == s[right]) {
-            if (right + 1 - left >= k) {
-                intervals.push_back({left, right + 1});
-                break;
+        int n = s.size(), lastEnd = 0, count = 0;
+
+        for (int center = 0; center < 2 * n; center++) {
+            int left = center / 2;
+            int right = left + center % 2;
+
+            while (left >= 0 && right < n && s[left] == s[right]) {
+                if (right - left + 1 >= k) {
+                    int end = right + 1;
+
+                    if (left >= lastEnd)
+                        lastEnd = end, count++;
+                    else
+                        lastEnd = min(lastEnd, end);
+
+                    break;
+                }
+                left--, right++;
             }
-            left--; right++;
         }
+
+        return count;
     }
-    for (vector<int>& v: intervals) {
-        if (v[0] >= last) {
-            last = v[1];
-            ans++;
-        }
-        else
-            if (v[1] < last)
-                last = v[1];
-    }
-    return ans;
-}
 };
